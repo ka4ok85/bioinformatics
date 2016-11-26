3cf48ccab6bf5386b507cdb40d3f79ab39a00237
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2016-11-26 11:11:52 -0500

    added recursive approach for permutation problem

--- Permutation/src/com/github/ka4ok85/bioinformatics/Run.java
+++ Permutation/src/com/github/ka4ok85/bioinformatics/Run.java
@@ -7,6 +7,7 @@ public class Run {
 	 */
 	public static void main(String[] args) {
 
+		// non-recursive approach
 		int[] elements = {1,2,3,4};
 
 		System.out.println(Permutation.factorial(elements.length));
@@ -14,6 +15,21 @@ public class Run {
 		Permutation permutation = new Permutation(elements);
 		permutation.generate();
 
+
+		// recursive approach
+		int[] elements2 = {6,5,4,3,2,1};
+
+		System.out.println(PermutationRecursive.factorial(elements2.length));
+
+		PermutationRecursive permutationRecursive = new PermutationRecursive();
+		for (int i = 0; i < elements2.length; i++) {
+			permutationRecursive.generate(
+					elements2[i], 
+					PermutationRecursive.removeElement(elements2, elements2[i]),
+					""
+
+			);
+		}
 	}
 
 }
