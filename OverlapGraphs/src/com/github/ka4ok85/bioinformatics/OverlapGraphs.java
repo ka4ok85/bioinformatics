8fbcad4a54902bdcb67cc15b8c38052af7aeda50
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2016-12-01 21:28:30 -0500

    finished Overlap Graphs problem

--- OverlapGraphs/src/com/github/ka4ok85/bioinformatics/OverlapGraphs.java
+++ OverlapGraphs/src/com/github/ka4ok85/bioinformatics/OverlapGraphs.java
@@ -6,99 +6,70 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 public class OverlapGraphs {
 
-	public void run() {
-		
-		HashMap<String, String> nodesList = new HashMap<String, String>();
-		
-		HashMap<String, ArrayList<String>> suffixesList = new HashMap<String, ArrayList<String>>();
-		
-		
-		
-		String filePath = "C:\\files\\data\\overlapgraph.txt";
+	private HashMap<String, String> nodesList = new HashMap<String, String>();
+	
+	// reads FASTA data into HashMap
+	public void readFile() {
+		String filePath = "D:\\overlapgraph.txt";
 		try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
 			String line;
 			String key = "";
-			Boolean isKey = true;
-			
+			String value = "";
 			while ((line = br.readLine()) != null) {
-		        //System.out.println(line);
-		        if (isKey == false) {
-		        	nodesList.put(key, line);
-		        	isKey = true;
-		        } else {
-			        key = line.substring(1);
-		        	isKey = false;
-		        	
-		        	
-		        }
-		    }
-
-			for (Map.Entry<String, String> entry : nodesList.entrySet()) {
-				// can be used for duplicates check
-//System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-			}
-			
-			String suffix = "";
-			ArrayList<String> descriptionLines;
-			for (Map.Entry<String, String> entry : nodesList.entrySet()) {
-				suffix = entry.getValue().substring(entry.getValue().length()-3, entry.getValue().length());
-				//System.out.println(suffix);
-				
-				if (suffixesList.containsKey(suffix)) {
-					descriptionLines = suffixesList.get(suffix);
-				} else {
-					descriptionLines = new ArrayList<String>();
-				}
-				
-				descriptionLines.add(entry.getKey());
-				
-				suffixesList.put(suffix, descriptionLines);
-			}
-			
-
-			for (Map.Entry<String, ArrayList<String>> entry : suffixesList.entrySet()) {
-				//System.out.println(entry.getKey());
-				//System.out.println(entry.getValue());
-			}
-			
-//System.out.println("-------");
-			String prefix;
-			for (Map.Entry<String, String> entry : nodesList.entrySet()) {
-				// can be used for duplicates check
-				//System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-				prefix = entry.getValue().substring(0, 3);
-				//System.out.println(prefix);
-				if (suffixesList.containsKey(prefix)) {
-					
-
-					
-					for (String description : suffixesList.get(prefix)) {
-						if (description.equals(entry.getKey()) == false) {
-							System.out.println(description + " " + entry.getKey());
-						}
+				if (line.substring(0, 1).equals(">")) {
+					if (value.equals("") == false) {
+						nodesList.put(key, value);
+						value = "";
 					}
-					//System.out.println(entry.getKey() + " " + suffixesList.get(prefix));
+
+					key = line;
+				} else {
+					value = value + line;
 				}
-				
-				
-				
 			}
-			
+
+			nodesList.put(key, value);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-
 			e.printStackTrace();
 		}
 		
-
-		
 	}
+	
+	public void run() {
+		HashMap<String, ArrayList<String>> suffixesList = new HashMap<String, ArrayList<String>>();
+		String suffix = "";
+		ArrayList<String> descriptionLines;
+		// build unique suffixes list
+		for (Map.Entry<String, String> entry : nodesList.entrySet()) {
+			suffix = entry.getValue().substring(entry.getValue().length()-3, entry.getValue().length());
+			if (suffixesList.containsKey(suffix)) {
+				descriptionLines = suffixesList.get(suffix);
+			} else {
+				descriptionLines = new ArrayList<String>();
+			}
+				
+			descriptionLines.add(entry.getKey());
+			suffixesList.put(suffix, descriptionLines);
+		}
+			
+		String prefix;
+		// compare prefix and suffixes list
+		for (Map.Entry<String, String> entry : nodesList.entrySet()) {
+			prefix = entry.getValue().substring(0, 3);
+			if (suffixesList.containsKey(prefix)) {
+				for (String description : suffixesList.get(prefix)) {
+					if (description.equals(entry.getKey()) == false) {
+						System.out.println(description.substring(1) + " " + entry.getKey().substring(1));
+					}
+				}
+			}
+		}
+	}
+
 }
--- OverlapGraphs/src/com/github/ka4ok85/bioinformatics/Run.java
+++ OverlapGraphs/src/com/github/ka4ok85/bioinformatics/Run.java
@@ -1,9 +1,13 @@
 package com.github.ka4ok85.bioinformatics;
 
+/*
+ * return: The adjacency list corresponding to O3. You may return edges in any order.
+ */
 public class Run {
 
 	public static void main(String[] args) {
 		OverlapGraphs overlapGraphs = new OverlapGraphs();
+		overlapGraphs.readFile();
 		overlapGraphs.run();
 	}
 
