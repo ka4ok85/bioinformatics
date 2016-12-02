9a0e3ce66eb6580fda8d34d2b7692408af484f1b
ka4ok85 <makovetsky.evgeny@gmail.com>
2016-12-02 17:16:58 -0500

    perfect matchings progress

--- PerfectMatchings/src/com/github/ka4ok85/bioinformatics/PerfectMatchings.java
+++ PerfectMatchings/src/com/github/ka4ok85/bioinformatics/PerfectMatchings.java
@@ -13,13 +13,94 @@ import java.util.Map.Entry;
 public class PerfectMatchings {
 
 	private HashMap<Integer, List<Integer>> edges = new HashMap<Integer, List<Integer>>();
+	
+	private HashMap<Integer, List<Integer>> clonedEdges = new HashMap<Integer, List<Integer>>();
+	
 	private List<Integer> nodes;
 	private int count = 0;
+	private int step = 1;
+	
+	
+	
+	
+	
+	public PerfectMatchings(String string) {
+		char[] charArray = string.toCharArray();
+		nodes = new ArrayList<Integer>();
+		int index = 1;
+		for (char c : charArray) {
+			nodes.add(index);
+			index++;
+		}
+		
+		
+		index = 1;
+		int compareIndex = 1;
+		for (char character : charArray) {
+			for (char compareCharacter : charArray) {
+				if (index != compareIndex) {
+					//System.out.println(character + "_" + compareCharacter);
+					if (
+							(character == "A".charAt(0) && compareCharacter == "U".charAt(0)) ||
+							(character == "U".charAt(0) && compareCharacter == "A".charAt(0)) ||
+							(character == "C".charAt(0) && compareCharacter == "G".charAt(0)) ||
+							(character == "G".charAt(0) && compareCharacter == "C".charAt(0))
+					) {
+						//System.out.println(character + "_" + compareCharacter);
+						List<Integer> tmp = edges.get(index);
+						if (tmp == null) {
+							tmp = new ArrayList<Integer>();
+						}
+						//System.out.println(tmp);
+						tmp.add(compareIndex);
+						edges.put(index, new ArrayList<Integer>(tmp));
+					}
+				}
+				compareIndex++;
+			}
+			index++;
+			compareIndex = 1;
+		}
+		
+		for (Entry<Integer, List<Integer>> entry : edges.entrySet()) {
+//System.out.println(entry.getKey());
+//System.out.println(entry.getValue());
+		}
+
+		
+		
+		//edges.put(1, new ArrayList<Integer>(Arrays.asList(4, 7, 10)));		
+		
+		//System.out.println(nodes);
+	}
+
+	public HashMap<Integer, List<Integer>> getClonedEdges() {
+		return clonedEdges;
+	}
+
+	public HashMap<Integer, List<Integer>> getClonedHashMap() {
+		
+		HashMap<Integer, List<Integer>> clone = new HashMap<Integer, List<Integer>>();
+		for (Entry<Integer, List<Integer>> entry : edges.entrySet()) {
+			
+			
+			ArrayList<Integer> clonedList = new ArrayList<Integer>(entry.getValue());
+			clone.put(entry.getKey(), clonedList);
+			
+			//System.out.println(edges.get(entry.getKey()));
+			
+
+
+		}
+		
+		
+		return clone;
+	}
 	
 	public void buildEdges() {
 		
 		
-
+/*
 		
 		edges.put(1, new ArrayList<Integer>(Arrays.asList(4, 7, 10)));
 		edges.put(2, new ArrayList<Integer>(Arrays.asList(3, 8)));
@@ -31,22 +112,21 @@ public class PerfectMatchings {
 		edges.put(8, new ArrayList<Integer>(Arrays.asList(2, 6)));
 		edges.put(9, new ArrayList<Integer>(Arrays.asList(4, 7, 10)));
 		edges.put(10, new ArrayList<Integer>(Arrays.asList(1, 5, 9)));
-
-		
-
-		/*
-		edges.put(1, Arrays.asList(4, 7, 10));
-		edges.put(2, Arrays.asList(3, 8));
-		edges.put(3, Arrays.asList(2, 6));
-		edges.put(4, Arrays.asList(1, 5, 9));
-		edges.put(5, Arrays.asList(4, 7, 10));
-		edges.put(6, Arrays.asList(3, 8));
-		edges.put(7, Arrays.asList(1, 5, 9));
-		edges.put(8, Arrays.asList(2, 6));
-		edges.put(9, Arrays.asList(4, 7, 10));
-		edges.put(10, Arrays.asList(1, 5, 9));
 */
+		//HashMap<Integer, List<Integer>> clone = new HashMap<Integer, List<Integer>>();
+		for (Entry<Integer, List<Integer>> entry : edges.entrySet()) {
+			
+			
+			ArrayList<Integer> clonedList = new ArrayList<Integer>(entry.getValue());
+			clonedEdges.put(entry.getKey(), clonedList);
+			
+			//System.out.println(edges.get(entry.getKey()));
+			
 
+
+		}
+
+/*
 		nodes = new ArrayList<Integer>();
 		nodes.add(1);
 		nodes.add(2);
@@ -58,26 +138,68 @@ public class PerfectMatchings {
 		nodes.add(8);
 		nodes.add(9);
 		nodes.add(10);
+*/
 	}
 	
 	public void generate(List<Integer> currentNodes, HashMap<Integer, List<Integer>> currentEdges, int node, int connectTo, String result) {
-		System.out.println(result);
-		System.out.println("NODE" + node);
-		
+		//System.out.println(result);
+		//System.out.println("STEP" + step);
+		//System.out.println("NODE" + node);
+		//System.out.println("NODE connectTo" + connectTo);
 
+		if (result.equals("")) {
+			//currentEdges = new HashMap<Integer, List<Integer>>(edges);
+			//System.out.println("+++++++++++");
+			/*
+			currentEdges = new HashMap<Integer, List<Integer>>();
+			edges.put(1, new ArrayList<Integer>(Arrays.asList(4, 7, 10)));
+			edges.put(2, new ArrayList<Integer>(Arrays.asList(3, 8)));
+			edges.put(3, new ArrayList<Integer>(Arrays.asList(2, 6)));
+			edges.put(4, new ArrayList<Integer>(Arrays.asList(1, 5, 9)));
+			edges.put(5, new ArrayList<Integer>(Arrays.asList(4, 7, 10)));
+			edges.put(6, new ArrayList<Integer>(Arrays.asList(3, 8)));
+			edges.put(7, new ArrayList<Integer>(Arrays.asList(1, 5, 9)));
+			edges.put(8, new ArrayList<Integer>(Arrays.asList(2, 6)));
+			edges.put(9, new ArrayList<Integer>(Arrays.asList(4, 7, 10)));
+			edges.put(10, new ArrayList<Integer>(Arrays.asList(1, 5, 9)));
+			*/
+			
+			//currentEdges = getClonedEdges();
+			
+			for (Entry<Integer, List<Integer>> entry : getClonedEdges().entrySet()) {
+				ArrayList<Integer> clonedList = new ArrayList<Integer>(entry.getValue());
+				currentEdges.put(entry.getKey(), clonedList);
+			}
+			
+			
+
+		}
+
+		//for (Entry<Integer, List<Integer>> entry : currentEdges.entrySet()) {
+			//System.out.println("@@@" + entry.getKey());
+			//System.out.println("@@@" + entry.getValue());
+
+		//}
 		
 		if (connectTo == 0) {
 			List<Integer> possibleEdges = currentEdges.get(node);
+			//System.out.println("OOPS: " + possibleEdges);
+			
+			
+			
+			//HashMap<Integer, List<Integer>> tmp;
+			//tmp = new HashMap<Integer, List<Integer>>(currentEdges);
 			for (Integer nextNode : possibleEdges) {
-				System.out.println("--- CALL itself nextNode" + nextNode);
-				System.out.println("--- CALL itself nextNode" + currentNodes);
-				
+				//System.out.println("--- CALL itself nextNode" + nextNode);
+				//System.out.println("--- CALL itself nextNode" + currentNodes);
 				
 				
 				
 				//generate(currentNodes, currentEdges, node, nextNode, result);
-				System.out.println("parent generate call");
+				//System.out.println("parent generate call");
+				//currentEdges = edges;
 				generate(new ArrayList<Integer>(currentNodes), new HashMap<Integer, List<Integer>>(currentEdges), node, nextNode, result);
+				//generate(new ArrayList<Integer>(currentNodes), new HashMap<Integer, List<Integer>>(tmp), node, nextNode, result);
 			}
 		}
 		
@@ -86,12 +208,27 @@ public class PerfectMatchings {
 			//List<Integer> possibleEdges = currentEdges.get(node);
 			//for (Integer nextNode : possibleEdges) {
 				int nextNode = connectTo;
-				System.out.println("nextNode" + nextNode);
+				//System.out.println("nextNode" + nextNode);
+				//System.out.println("nextNode node" + node);
+				//System.out.println("step" + step);
 				if (nextNode == 0) {
+					
+					//System.out.println("RETURN: currentNodes="  + currentNodes);
+					//System.out.println("RETURN: step="  + step);
+					if (step != 1) {
+						currentNodes.remove(Integer.valueOf(node));
+						result += node + " " + currentNodes.get(0) + " - ";
+						//System.out.println("RETURN: RESULT="  + result);
+						//System.out.println("------------");
+						System.out.println(result);
+						step = 1;
+						count++;
+					}
+					
 					return;
 				}
-System.out.println("***" + node);
-System.out.println("***" + currentNodes);
+//System.out.println("***" + node);
+//System.out.println("***" + currentNodes);
 				//generate(currentNodes, currentEdges, nextStepNode, 0, result);
 				if (currentNodes.contains(nextNode) == false) {
 					// ???
@@ -101,7 +238,7 @@ System.out.println("***" + currentNodes);
 				
 				currentEdges.remove(node);
 				currentEdges.remove(nextNode);
-System.out.println(currentNodes);				
+//System.out.println(currentNodes);				
 				currentNodes.remove(Integer.valueOf(node));
 				
 				currentNodes.remove(Integer.valueOf(nextNode));
@@ -112,7 +249,7 @@ System.out.println(currentNodes);
 					entry.getValue().remove(Integer.valueOf(node));
 				}
 				
-				System.out.println(currentNodes);
+				//System.out.println(currentNodes);
 				
 				int nextStepNode = 0;
 				for (Integer index: currentNodes) {
@@ -120,30 +257,38 @@ System.out.println(currentNodes);
 					break;
 				}
 				
-				System.out.println("nextStepNode" + nextStepNode);
+				//System.out.println("nextStepNode" + nextStepNode);
 				
 				if (nextStepNode == 0) {
 					
 
 					
 						result += node + " " + nextNode + " - ";
-						System.out.println("------------");
+						//System.out.println("------------");
 						System.out.println(result);
-						System.out.println("stop");
+						//System.out.println("stop");
+						step = 1;
 						count++;
+						
 						return;
 					//break;
 				} else {
 				
 					result += node + " " + nextNode + " - ";
-					System.out.println("child generate call");
-					System.out.println(currentNodes);
+					//System.out.println("child generate call");
+					//System.out.println(currentNodes);
+					step++;
+					//for (Entry<Integer, List<Integer>> entry : currentEdges.entrySet()) {
+//						System.out.println("###" + entry.getKey());
+						//System.out.println("###" + entry.getValue());
+
+					//}
 					generate(currentNodes, currentEdges, nextStepNode, 0, result);
 					//break;
 				}
 			//}
 		} else {
-			System.out.println("???");
+			//System.out.println("???");
 		}
 		//currentEdges.n
 		/*
--- PerfectMatchings/src/com/github/ka4ok85/bioinformatics/Run.java
+++ PerfectMatchings/src/com/github/ka4ok85/bioinformatics/Run.java
@@ -1,11 +1,18 @@
 package com.github.ka4ok85.bioinformatics;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Run {
 
 	public static void main(String[] args) {
-		PerfectMatchings perfectMatchings = new PerfectMatchings();
+//AGCUAGUCAU		
+//GGUCGUACAU
+//GGUCGAACAGAUCAUUAACAAGUGCCUGCGUCCUGAGUCAAAUUCGCUUCGCCUCGCCUGGACAAGGUGG
+		
+		PerfectMatchings perfectMatchings = new PerfectMatchings("GGUCGUACAU");
 		perfectMatchings.buildEdges();
-		perfectMatchings.generate(perfectMatchings.getNodes(), perfectMatchings.getEdges(), 1, 0, "");
+		perfectMatchings.generate(perfectMatchings.getNodes(), new HashMap<Integer, List<Integer>>(perfectMatchings.getEdges()), 1, 0, "");
 
 		System.out.println(perfectMatchings.getCount());
 	}
